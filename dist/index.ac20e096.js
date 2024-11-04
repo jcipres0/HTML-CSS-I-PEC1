154e5842bbbf836aa368f1689fb2b41a688b9a80
Juan Cipres Serrano <jcipres0@uoc.edu>
2024-11-04 10:43:20 +0100

    6th cambios para babel

--- dist/index.ac20e096.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".slider"),t=document.querySelectorAll(".slider-inner");let n=0,l=0,r=100/t.length;setInterval(()=>{!function(){if(l>=t.length-1){l=0,n=0,e.style.transform=`translate(-${n}%)`,e.style.transition="none";return}l++,n+=r,e.style.transform=`translate(-${n}%)`,e.style.transition="all ease .6s"}()},3e3);
-//# sourceMappingURL=index.ac20e096.js.map
